ff335f3b25e97e7041ae431d50738ee04e13f9fd
ChenTanyi <chentanyi@gmail.com>
2020-09-11 18:04:59 +0800

    feat: add some script for ubuntu

---
+++ nvidia-driver.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# ubuntu-drivers devices
+# sudo ubuntu-drivers autoinstall
+
+# MOK
+# sudo mokutil --import /var/lib/shim-signed/mok/MOK.der
+
+# sudo prime-select nvidia # intel
---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# ~/.bashrc https://gist.github.com/ChenTanyi/8bdb39fb9fcadbd1c9307a83d0874f07
+# azure.bashrc https://gist.github.com/ChenTanyi/bdf9f3b86baf2ffaea0921a03c89a028
+
+# ~/.inputrc https://gist.github.com/ChenTanyi/e98d7fb87d7e18334e50bfb01b8817f1
+
+# ~/.gitconfig https://gist.github.com/ChenTanyi/45eec0c1f96efeee39d918a0ee14c383
+
+# ~/.clang-format https://gist.github.com/ChenTanyi/1f59bfeef218386f7ce8279c83305831
+# ~/.style.yapf https://gist.github.com/ChenTanyi/fa37e1aea64c15d90554675dca422634
+
+# ngrok https://ngrok.com/
